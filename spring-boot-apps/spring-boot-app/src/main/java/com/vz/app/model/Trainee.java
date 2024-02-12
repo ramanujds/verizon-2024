bda2baa8cc3872598abbbad0b09f68b9c5bfcd3d
Ramanuj Das <ramanujds9@gmail.com>
2024-02-12 11:43:19 +0530

    added spring boot project

---
+++ spring-boot-apps/spring-boot-app/src/main/java/com/vz/app/model/Trainee.java
@@ -0,0 +1,50 @@
+package com.vz.app.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+public class Trainee {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int id;
+	private String name;
+	private String email;
+	private String location;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	@Override
+	public String toString() {
+		return "Trainee [id=" + id + ", name=" + name + ", email=" + email + ", location=" + location + "]";
+	}
+	
+	
+	
+
+}
