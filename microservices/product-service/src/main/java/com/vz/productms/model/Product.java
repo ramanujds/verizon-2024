ab2860de7874fca6504513ee24aaab2a825a96e5
Ramanuj Das <ramanujds9@gmail.com>
2024-02-13 10:51:03 +0530

    Added some products using sql

--- microservices/product-service/src/main/java/com/vz/productms/model/Product.java
+++ microservices/product-service/src/main/java/com/vz/productms/model/Product.java
@@ -1,24 +1,26 @@
 package com.vz.productms.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @Entity
+@Table(name = "product")
 public class Product {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "product_id")
     private int productId;
+    @Column(name = "product_name")
     private String productName;
 
+    @Column(name = "description")
     private String description;
 
+    @Column(name = "price")
     private float price;
 
 }
